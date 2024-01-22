654ad07bebad6b8ad37469d707f0f03d0c223da3
dd <14729608136@163.com>
2024-01-22 15:35:11 +0800

    测试huskey

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
-const fun = () => {
-  console.log('hahha')
+
+const fun = () =>
+ {
+
+console.log('hahha')
+
 }
 
 fun()
