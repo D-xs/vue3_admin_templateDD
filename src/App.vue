5b27ff0916f7375578c463462c4e4aa3a74df3c0
dd <14729608136@163.com>
2024-01-22 16:25:31 +0800

    build: 新增element-plus组件库以及相关图标

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
 <template>
   <h2>我是App组件</h2>
-  <div class="com"></div>
+  <el-button type="primary">Primary</el-button>
 </template>
 
 <script lang="ts" setup name="App">
-let fun = () => {
-  console.log('ss')
-}
-fun()
+
 </script>
 
-<style scoped lang="scss">
-.com {
-  background-color: skyblue;
-  width: 200px;
-  height: 200px;
-  border-radius: 10px;
-}
-</style>
+<style scoped lang="scss"></style>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
+// 引入element-plus
+import ElementPlus from 'element-plus'
+// 引入根组件
 import App from './App.vue'
-
-createApp(App).mount('#app')
-
-const fun = () => {
-  console.log('wokankan')
-}
-
-fun()
+// 引入element中文包
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+// 引入组件样式
+import 'element-plus/dist/index.css'
+const app = createApp(App)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+app.mount('#app')
