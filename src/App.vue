0ecccf958fd7b7e9f12a0aaa9c67c1786e19ce62
dd <14729608136@163.com>
2024-01-22 15:23:31 +0800

    提交没有格式化的代码

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>我是App组件</h2>
+  <div class="com"></div>
+</template>
+
+<script lang="ts" setup name="App">
+let fun = () => {
+
+  console.log('ss')
+
+}
+fun()
+</script>
+
+<style scoped lang="scss">
+.com {
+  background-color: skyblue;
+  width: 200px;
+  height: 200px;
+  border-radius: 10px;
+}
+</style>
